4b77006b37e9a85ca9e0cbb2e4423cecae95e798
Harish Yadav <harish.yadav@outlook.in>
2026-09-05 00:49:41 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_dict = {}
+        for idx, num in enumerate(nums):
+            if target-num in index_dict:
+                return [index_dict[target-num], idx]
+            index_dict[num] = idx
+        return []
